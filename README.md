f523349542e3b5850e1f55d34aca7ee459c39384
chidiugonna <chidi_ugonna@hotmail.com>
2021-03-05 21:01:37 -0700

    first working iteration

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # maddenXCP
 Customized version of xcp-engine version v1.2.3 to enable censoring of adjacent volumes
+
+## Changes to xcp-engine
+
+`${XCPEDIR}/utils/fd.R` main code has been commented out to prevent creation of the .../confound2/mc/${SUB}_${SES}_fd.1D file which is created from the 6 motion regressors provided by fmriprep.
+
+'${XCPEDIR}/utils/generate_confmat.R' opportunistically create .../confound2/mc/${SUB}_${SES}_fd.1D as a copy of .../confound2/mc/${SUB}_${SES}_relRMS.1D which is created from the `framewise_displacement` field in the fMRIPREP confounds.tsv file.
+
+## Workflow of maddenXCP
+
+The python program `modifyConfounds.py` can be called to artificially flag volumes before and after an outlier volume that exceeds a framewise displacement threshold. The framewise displacement value for these neighboring timepoints is then replaced by an inflated value that will be subsequently picked up by xcpEngine as an outlier.
+Please note that this functionality has only been created as an inelegant workaround to the above problem. Any functionality in xcpEngine that relies on accurate FD values will be compromised. This is likely to include some of the QCFC metrics that are calculated for each subject. A recommendation then is to run your denoising pipeline twice - once to derive the censored volumes using this custom version of xcpEngine and a second time with the proper stable version of xcpEngine to obtain gold standard outputs.
+
+### Example
+Using a framewise displacement threshold of 0.3mm, I intend to flag 2 preceding volumes to outlier volumes to be scrubbed and 1 subsequent volume.
+I will replace the FD values of these volumes to be 0.5 mm so that the are definitely scrubbed by xcpEngine.
+
+So in a toy example of 10 volumes with FD as follows [ 0.1 0.02 0.004 0.15 0.12 0.32 0.2 0.12 0.11 0.15]
+We expect the 6th volume to be scrubbed and thus we want to also scrub volumes 4 and 5 and 7.
+Thus after running this through `modifyConfounds.py` as follows
+
+`modifyConfounds.py ${COUNFOUND_FILE} --confound_out=${BACKUP_NAME} --fd_thresh=0.3 --fd_replacement=0.5 --vols_before=2 --vols_after=1`
+
+We get the following in our confounds file which can now be passed onto xcp for scrubbing:
+
+`[ 0.1 0.02 0.004 0.5 0.5 0.32 0.5 0.12 0.11 0.15]`
---
+++ customR/generate_confmat.R
@@ -0,0 +1,115 @@
+#! /usr/bin/env Rscript
+
+################################################################### 
+#  ⊗  ⊗  ⊗  ⊗  ⊗  ⊗  ⊗  ⊗  ⊗  ⊗  ⊗  ⊗  ⊗  ⊗  ⊗  ⊗  ⊗  ⊗  ⊗  ⊗  ⊗  #
+###################################################################
+
+###################################################################
+# THE FUNCTIONALITY OF generate matrices.R IS CRUDE AND LIKELY TO CHANGE IN
+# THE FUTURE.
+# You are advised to avoid excessive dependency on this script
+# for the time being.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+###################################################################
+
+###################################################################
+# Load required libraries
+###################################################################
+library(optparse)
+library(pracma)
+
+###################################################################
+# Parse arguments to script, and ensure that the required arguments
+# have been passed.
+###################################################################
+option_list = list(
+   make_option(c("-i", "--mat"), action="store", default='null', type='character',
+              help="Path to the first matrix to be merged or operated upon"),
+   make_option(c("-j", "--js"), action="store", default='null', type='character',
+              help=" Specify the confound require  to pull out  from frmriprep ouput
+                  
+                comment neeeded here later 
+              
+                  etc."),
+   make_option(c("-o", "--out"), action="store", default=NA, type='character',
+              help="Output path")
+)
+opt = parse_args(OptionParser(option_list=option_list))
+
+if (is.na(opt$out)) {
+   cat('User did not specify an output path.\n')
+   cat('Use generate_confmat.R -h for an expanded usage menu.\n')
+   quit()
+}
+
+in1 <- opt$mat
+in2 <- opt$js
+out <- opt$out
+
+###################################################################
+###################################################################
+outmat=0
+
+## read the confoundmatrix
+mat1 <- read.table(in1,sep = '\t', header =TRUE)
+b=colnames(mat1)
+
+ #if ( b[1]=='csf' ) {
+
+      if (in2 == 'csf') {
+      outmat=mat1$csf
+      outmat[is.na(outmat)]=0
+      }  else if (in2 == 'wm' ) {
+      outmat=mat1$white_matter
+      outmat[is.na(outmat)]=0
+      }  else if (in2 == 'gsr' ) {
+      outmat=mat1$global_signal
+      outmat[is.na(outmat)]=0
+     } else if ( in2 == 'tCompCor' ) {
+     outmat <- mat1[ , grepl('t_comp_cor',names(mat1))]
+     outmat=outmat[,1:5]
+     outmat[is.na(outmat)]=0
+     } else if ( in2 == 'aCompCor' ) {
+     outmat <- mat1[ , grepl('a_comp_cor', names(mat1))]
+     outmat=outmat[,1:5]
+     outmat[is.na(outmat)]=0
+     } else if ( in2 == 'aroma' ) {
+     outmat <- mat1[ , grepl('aroma', names(mat1))]
+     outmat[is.na(outmat)]=0
+     } else if ( in2 == 'Cosine') { 
+     outmat = mat1[ , grepl( 'cosine', names(mat1))]
+     outmat[is.na(outmat)]=0
+     } else if ( in2 == 'rps' ) {
+     outmat=cbind(mat1$rot_x,mat1$rot_y,mat1$rot_z,mat1$trans_x,mat1$trans_y,mat1$trans_z)
+     outmat[is.na(outmat)]=0
+     } else if (in2 == 'stdVARS') { 
+      outmat=mat1$std_dvars
+      outmat=suppressWarnings(as.numeric(as.character(outmat)))
+      outmat[is.na(outmat)]=0
+      outmat=as.factor(outmat)
+     } else if (in2 == 'allVARS') {
+     outmat=mat1[ , grepl( 'dvars' , names(mat1) ) ]
+     outmat=suppressWarnings(as.numeric(as.character(outmat)))
+     outmat[is.na(outmat)]=0
+     outmat=as.factor(outmat)
+     } else if ( in2 == 'rms' ) {
+     mat2=(cbind(mat1$trans_x,mat1$trans_y,mat1$trans_z))^2
+     outmat=sqrt(rowSum(mat2)/3)
+     outmat[is.na(outmat)]=0  
+    } else if (in2 == 'fd') {
+     outmat = mat1$framewise_displacement
+      outmat=suppressWarnings(as.numeric(as.character(outmat)))
+      outmat[is.na(outmat)]=0
+      outmat=as.factor(outmat)
+      ## CPU 03/03/2021 - output fd file as well as rms file
+      ## BEGIN
+      newout <- paste0(unlist(strsplit(out,"relRMS"))[1],"fd.1D",NULL)
+      write.table(outmat,file=newout, col.names = F, row.names=F,quote=F)
+      ## END
+      } else  {
+      sprintf("the input is not available yet") 
+   }
+#}
+
+write.table(outmat,file=out, col.names = F, row.names=F,quote=F)
